966392266b4cc18263153beb9c3bd17c3cff52cf
Reason2Code <iamphupq@gmail.com>
2026-05-05 20:35:50 +0700

    #

--- HotelManagementSystem/Properties/Settings.Designer.cs
+++ HotelManagementSystem/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace HotelManagementSystem.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost,1436;Initial Catalog=HotelDB;Persist Security Info=True;Use" +
-            "r ID=phu;Password=12349977SQLserver@#;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=HotelDB;Persist Security Info=True;User ID=" +
+            "ad;Password=12345678@Aa;")]
         public string ConnectionString {
             get {
                 return ((string)(this["ConnectionString"]));
